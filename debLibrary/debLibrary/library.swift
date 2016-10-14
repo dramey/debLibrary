fccc9c2a41427ea81eeaa44e759e31fb3292228b
Deb Ramey <debramey.dr@gmail.com>
2016-10-13 21:25:13 -0400

    added somethings to my library

---
+++ debLibrary/debLibrary/library.swift
@@ -0,0 +1,19 @@
+//
+//  library.swift
+//  debLibrary
+//
+//  Created by Deb Ramey on 10/13/16.
+//  Copyright © 2016 Deb Ramey. All rights reserved.
+//
+
+import Foundation
+class LibraryOfBooks{
+    
+    var myBooks: [String:String] =  [
+        "123":"Twilight",
+        "234":"New Moon",
+        "345":"Eclipse",
+        "456":"Breaking Dawn",
+        "567":"The Phantom Tollbooth"
+        
+]
